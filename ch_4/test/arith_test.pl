eaed8e238ca31fcb2582f922f0f436e3c7a12aa4
Simon Coffey <simon@urbanautomaton.com>
2017-02-09 11:20:56 +0000

    fiddling

--- ch_4/test/arith_test.pl
+++ ch_4/test/arith_test.pl
@@ -17,6 +17,9 @@ test(evaluate_for_multiple_succ) :-
 test(evaluate_for_arithmetic_expression) :-
   evaluate(pred(succ(0)), 0).
 
+test(evaluate_pred_zero) :-
+  evaluate(pred(pred(pred(0))), 0).
+
 test(evaluate_for_something_complex) :-
   evaluate(
     if(iszero(succ(pred(0))), succ(0), succ(succ(0))),
@@ -45,7 +48,13 @@ test(evaluate_for_something_ridiculous_or_at_least_as_ridiculous_as_i_could_be_b
 
 :- begin_tests(reduce).
 
-test(reduce_if_by_only_a_single_step, all(X = [if(true, false, false)])) :-
+test(reduce_if_true, [nondet]) :-
+  reduce(if(true, 0, succ(0)), 0).
+
+test(reduce_if_false, [nondet]) :-
+  reduce(if(false, 0, succ(0)), succ(0)).
+
+test(reduce_if_guard, all(X = [if(true, false, false)])) :-
   reduce(
     if(if(true, true, true), false, false),
     X
