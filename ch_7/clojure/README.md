bbff096ac86e85a8cb13530ff029e8d7c27c4c01
Simon Coffey <simon@urbanautomaton.com>
2017-05-05 21:24:51 +0100

    [Ch 7] I accidentally a clojure version

---
+++ ch_7/clojure/README.md
@@ -0,0 +1,7 @@
+# lambda
+
+An unfinished implementation of the untyped lambda calculus in clojure.
+
+## Usage
+
+Nah mate.
---
+++ ch_7/clojure/project.clj
@@ -0,0 +1,7 @@
+(defproject lambda "0.1.0-SNAPSHOT"
+  :description "An implementation of the untyped lambda calculus for computation.club"
+  :url "https://github.com/urbanautomaton/tapl/ch_7/clojure"
+  :license {:name "MIT License"
+            :url "http://www.opensource.org/licenses/mit-license.php"}
+  :dependencies [[org.clojure/clojure "1.8.0"],
+                 [instaparse  "1.4.5"]])
