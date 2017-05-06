323811dd38674a6ba58c57f85f08f22e57d632dd
Simon Coffey <simon@urbanautomaton.com>
2017-05-06 10:03:00 +0100

    [Ch 7] Put lambda parser tests in the test file

--- ch_7/clojure/test/lambda/core_test.clj
+++ ch_7/clojure/test/lambda/core_test.clj
@@ -2,6 +2,72 @@
   (:require [clojure.test :refer :all]
             [lambda.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(defn assert-eq [x y] (is (= x y)))
+
+(deftest test-lambda-parser
+  (testing "lambda term:"
+    (testing "variable"
+      (assert-eq
+        (lambda-parser "a")
+        [:expr [:var "a"]]))
+
+    (testing "parenthesised variable"
+      (assert-eq
+        (lambda-parser "(a)")
+        [:expr [:var "a"]]))
+
+    (testing "application"
+      (assert-eq
+        (lambda-parser "a b c")
+        [:expr
+         [:app
+          [:app [:var "a"] [:var "b"]]
+          [:var "c"]]] ))
+
+    (testing "parenthesised application"
+      (assert-eq
+        (lambda-parser "a (b c)")
+        [:expr
+         [:app
+          [:var "a"]
+          [:app
+           [:var "b"]
+           [:var "c"]]]]))
+
+    (testing "abstraction"
+      (assert-eq
+        (lambda-parser "λ.x x y")
+        [:expr
+         [:abs
+          [:var "x"]
+          [:app
+           [:var "x"]
+           [:var "y"]]]]))
+
+    (testing "application of abstractions"
+      (assert-eq
+        (lambda-parser "(λ.x x) (λ.y y)")
+        [:expr
+         [:app
+          [:abs
+           [:var "x"]
+           [:var "x"]]
+          [:abs
+           [:var "y"]
+           [:var "y"]]]]))
+
+    (testing "nested abstractions"
+      (assert-eq
+        (lambda-parser "λ.x λ.y λ.z x y z")
+        [:expr
+         [:abs
+          [:var "x"]
+          [:abs
+           [:var "y"]
+           [:abs
+            [:var "z"]
+            [:app
+             [:app
+              [:var "x"]
+              [:var "y"]]
+             [:var "z"]]]]]]))))
