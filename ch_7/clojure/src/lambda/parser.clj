bf82e8f844f49a9ac0d9bf33d467b356ccc2e32f
Simon Coffey <simon.coffey@futurelearn.com>
2017-05-09 17:31:51 +0100

    Incredibly wip trying to get records/protocols involved

--- ch_7/clojure/src/lambda/parser.clj
+++ ch_7/clojure/src/lambda/parser.clj
@@ -1,4 +1,15 @@
 (ns lambda.parser
-  (:require [instaparse.core :as insta]))
+  (:require [instaparse.core :as insta]
+            [lambda.terms :refer :all]))
 
-(def parse (insta/parser (clojure.java.io/resource "lambda.bnf")))
+(def parse
+ (recordize (insta/parser (clojure.java.io/resource "lambda.bnf"))))
+
+(rest [1 2 3])
+(last [1 2 3])
+
+(defn- recordize [term]
+  (case (first term)
+    :var (Var. (last term))
+    :app (apply App. (map recordize (rest term)))
+    :abs (Abs. (recordize (last term)))))
--- ch_7/clojure/src/lambda/terms.clj
+++ ch_7/clojure/src/lambda/terms.clj
@@ -1,1 +1,31 @@
 (ns lambda.terms)
+
+(defn free-variables [term]
+  (free-variables-in term '()))
+
+(defprotocol LambdaTerm
+  (free-variables-in [this bound]))
+
+(defrecord Var [name]
+  Object
+  (toString [_] name)
+  
+  LambdaTerm
+  (free-variables-in [this bound]
+    (if (some #{this} bound) '() (list this))))
+
+(defrecord App [left right]
+  Object
+  (toString [_] (str "(" left " " right ")"))
+  
+  LambdaTerm
+  (free-variables-in [this bound]
+    (concat (free-variables-in left bound) (free-variables-in right bound))))
+
+(defrecord Abs [param body]
+  Object
+  (toString [_] (str "λ." param " " body))
+  
+  LambdaTerm
+  (free-variables-in [this bound]
+    (free-variables-in body (conj bound param))))
